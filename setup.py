19d67133eca6139e90017b1cd993aece98050f37
mayank-vk <mayankvinaykejriwal@gmail.com>
2025-04-30 17:00:47 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements()->List[str]:
+    requirements_list:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_list.append(requirement)
+
+
+    except FileNotFoundError:
+        print("requirements not found")
+
+    return requirements_list
+
+
+setup(
+    name='NetworkSecurity',
+    version='0.0.1',
+    author='mayank',
+    author_email='mayankvinaykejriwal@GMAIL.COM',
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
