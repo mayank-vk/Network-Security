413189c7910860f42a34c5b496a8c4a2d12df525
mayank-vk <mayankvinaykejriwal@gmail.com>
2025-05-01 13:27:03 +0530

    etl pipeline

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='NetworkSecurity',
     version='0.0.1',
     author='mayank',
-    author_email='mayankvinaykejriwal@GMAIL.COM',
+    author_email='mayankvinaykejriwal@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements()
 )
